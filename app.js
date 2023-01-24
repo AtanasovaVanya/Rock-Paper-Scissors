3cf98e4e38a15a03d89137fd2f05ec485c660592
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2023-01-24 11:51:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const rock = 'Rock';
-const paper = 'Paper';
-const scissors = 'Scissors';
+function rockPaperScissors(playTurn) {
+    const rock = 'Rock';
+    const paper = 'Paper';
+    const scissors = 'Scissors';
+
+    if (playTurn == 'r' || playTurn == 'rock') {
+        playTurn = rock;
+    } else if (playTurn == 'p' || playTurn == 'paper') {
+        playTurn = paper;
+    } else if (playTurn == 's' || playTurn == 'scissors') {
+        playTurn = scissors;
+    } else {
+        console.log('Invalid input. Please try again...');
+    }
+
+    let computerRandomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerMove = '';
+
+    switch (computerRandomNumber) {
+        case 1: computerMove = 'Rock'; break;
+        case 2: computerMove = 'Paper'; break;
+        case 3: computerMove = 'Scissors'; break;
+    }
+}
